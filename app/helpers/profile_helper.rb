62189cac5ba6e33339b81b17a95cfaea0aadc86c
Rade Radišić <radisic.rade@gmail.com>
2016-04-14 12:31:50 +0200

    Refactored email sending code.

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,5 +1,2 @@
 module ProfileHelper
-  def self.user_profile(id)
-    user = User.find(id)
-  end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
   default :from => 'mail@example.com'
 
-  def welcome_email(id)
-    @user = User.find(id)
-    @url = 'http://localhost:3000'
-    mail(:to => @user.email, :subject => 'Hello!')
+  def verify_email(user)
+    @user = user
+    @url = request.domain
+    mail(:to => user.email, :subject => 'Hello from Basic!')
   end
 end
